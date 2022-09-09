1ae24d499afb58558750c969a84bb06301a15bd3
Анастасия Тибейкина <Nastya@kompas.lan>
2022-09-09 14:52:19 +0600

    add example 015 array for

---
+++ Example015_Array_For/Program.cs
@@ -0,0 +1,52 @@
+﻿int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+{
+Console.Write($"{array[i]}");
+}
+Console.WriteLine();
+}
+
+void selectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        int minPosition = i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) 
+            {
+                minPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }    
+}
+
+void selectionSortMax(int[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) 
+            {
+                maxPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }    
+}
+
+PrintArray(arr);
+selectionSort(arr);
+PrintArray(arr);
+selectionSortMax(arr);
+PrintArray(arr);
